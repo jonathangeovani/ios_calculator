60ba916fddc19ca3c47d75a87b3f78b519abb76e
jonathangeovani <dongeovanimenor@gmail.com>
2023-09-09 23:01:27 -0300

    add float numbers support and size limit

--- calculator.js
+++ calculator.js
@@ -1,6 +1,5 @@
-// TODO: ADD SUPORTE A CONTAS COM FLOAT VALUES;
-
 const result = document.getElementById('resultado');
+const windowWidth = window.innerWidth;
 
 let operand1, operand2, resultValue = 0;
 let operator;
@@ -43,8 +42,8 @@ function updateOperation(newOperator) {
         operand2 = result.innerText;
         resultValue = getResult(operand1,operator,operand2);
         operator = newOperator;
-        operand1 = resultValue;
-        result.innerHTML = resultValue;
+        operand1 = resultValue;        
+        result.innerHTML = checkLength(resultValue.toString());
         if (operator == '=') {
             isFirstOperation = true;
         }
@@ -55,11 +54,13 @@ function updateOperation(newOperator) {
 }
 
 function getResult(x, sign, y) {
+    x = x.replace(',','.');
+    y = y.replace(',','.');
     switch (sign) {
-        case '+': return parseInt(x) + parseInt(y);
-        case '-': return parseInt(x) - parseInt(y);
-        case '*': return parseInt(x) * parseInt(y);
-        case '/': return parseInt(x) / parseInt(y);
+        case '+': return parseFloat(x) + parseFloat(y);
+        case '-': return parseFloat(x) - parseFloat(y);
+        case '*': return parseFloat(x) * parseFloat(y);
+        case '/': return parseFloat(x) / parseFloat(y);
         case '=': return resultValue;
     }
 }
@@ -78,3 +79,42 @@ function percent () {
         result.innerText = percent.toString().replace('.',',');
     }
 }
+
+function checkLength(text) {
+    if (text.length >= 8) {
+        result.style.fontSize = changeSize('3.5rem');
+        result.innerText = text.slice(0,11);
+        return text.slice(0,11).replace('.',',');
+    } else if (text.length < 8) {
+        result.style.fontSize = changeSize('5rem');
+    }
+    return text.replace('.',',');
+}
+
+function changeSize(newSize) {
+    if (windowWidth > 360 && windowWidth <= 428) {
+        switch (newSize) {
+            case '3.5rem': return '3.5rem';
+            case '5rem': return newSize;
+        }
+    } else if (windowWidth <= 360 && windowWidth > 340) {
+        switch (newSize) {
+            case '3.5rem': return '3rem';
+            case '5rem': return '4rem';
+        }
+    } else if (windowWidth <= 340) {
+        switch (newSize) {
+            case '3.5rem': return '2rem';
+            case '5rem': return '3rem';
+        }
+    } else {
+        switch (newSize) {
+            case '3.5rem': return '2.8rem';
+            case '5rem': return '4.5rem';
+        }
+    }
+}
+
+document.addEventListener('click', () => {
+    result.innerText = checkLength(result.innerText);
+})
